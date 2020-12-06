98faa4811b6d92af9f1b3b8b833a0a207cfd95a4
bojanche <Boj@n@25122015>
2020-12-06 09:16:21 +0100

    First release

---
+++ aj_jca.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import os
+import time
+
+
+def open_browser(adresa, user, password):
+    path = os.getcwd()+'\chromedriver.exe'
+    options = webdriver.ChromeOptions()
+    # ignores S&ST no-valid-cert on the camera
+    options.add_argument('ignore-certificate-errors')
+    # Various performance improvements
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-proxy-server")
+    # keeps chrome open
+    options.add_experimental_option('detach', True)
+    # removes "Chrome is being controlled...blablabla" message
+    options.add_experimental_option("excludeSwitches", ['enable-automation']);
+    # full-screen mode for the app window
+    options.add_argument("--kiosk")
+    # Also I've added logging with -log switch...Same folder...
+    browser = webdriver.Chrome(executable_path=path, options=options, service_args=["--verbose", "--log-path=chromedirver.log"])
+    browser.get('https://'+adresa+':8443/?referrer=https%3A%2F%2F'+adresa+'%3A8443%2Fapp%2Fcom.tngtech.sastiot.feedback%2F')
+    loginElement = browser.find_element_by_id("Login_username")
+    loginElement.send_keys(user)
+    passwordElement = browser.find_element_by_id("Login_password")
+    passwordElement.send_keys(password)
+    loginButton = browser.find_element_by_id("Login_loginButton")
+    loginButton.click()
+    time.sleep(60)
+    browser.quit()
+
+
+# The main executable
+if __name__ == '__main__':
+    while True:
+        kamera_adresa = ''
+        kamera_username = ''
+        kamera_password = ''
+        open_browser(kamera_adresa, kamera_username, kamera_password)
